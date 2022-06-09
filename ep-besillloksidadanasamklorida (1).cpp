7e7d8455cd8dadd6331b6a99c88a116afe78a1fd
La Mardani Putra <106719645+Lamardaniputra@users.noreply.github.com>
2022-06-09 07:25:05 +0700

    Update ep-besillloksidadanasamklorida (1).cpp

--- ep-besillloksidadanasamklorida (1).cpp
+++ ep-besillloksidadanasamklorida (1).cpp
@@ -42,3 +42,58 @@ int main(){
     cin >> numb_lat;
     N = (pow(numb_lat,3)) * 3;
     volum = (float)N / numb_mol;
+  
+        numb_mol = densitas * (Na / Mr_asamklorida) * cm3_to_A3;
+    cout << "Masukkan panjang sel simulasi (eg. 5): ";
+    cin >> numb_lat;
+    N = (pow(numb_lat,3)) * 3;
+    volum = (float)N / numb_mol;
+
+    // panjang sel simulasi secara perhitungan
+    lx = pow(volum,(1.0/3.0));
+    ly = lx;
+    lz = lx;
+    if(lx < (2*rcut)){
+        cout << "sorry mazeh, sel simulasi kamu besar";
+        exit(0);
+    } else{
+        cout << "panjang sek simulasi baru: " << lx << endl;
+    }
+    lat = lx / (float)numb_lat;
+
+    // deklarasi variabel
+    float pi = 3.14;
+    
+    // deklarasi molekul besillloksida
+    float Fe1x[8], Fe2x[8], Fe1y[8], Fe2y[8], Fe1z[8], Fe2z[8];
+    float O1x[8], O2x[8], O3X[8], O1y[8], O2y[8], O3y[8], O1z[8], O2z[8], O3z [8];
+    float Clx[8], Cly[8], Clz[8];
+    float Hx[8], Hy[8], Hz[8];
+
+
+    /*            O   O   O
+                   \ / \ /
+                    F   F
+                  Cl - H  
+     molekul besillloksida dan asam klorida itu bentuk rigid
+   */
+
+    float rB_Fe1x = sin(180.0*pi/180.0) * 1.900;;
+    float rB_Fe2x = sin(180.0*pi/180.0) * 1.900;;
+    float rB_Fe1y = 0.0;
+    float rB_Fe2y = 0.0;
+    float rB_Fe1z = cos(180.0*pi/180.0) * 1.900;
+    float rB_Fe2z = cos(180.0*pi/180.0) * 1.900;
+
+    float rB_O1x = sin(180.0*pi/180.0) * 1.900;
+    float rB_O2x = sin(180.0*pi/180.0) * 1.900;
+    float rB_O3x = sin(180.0*pi/180.0) * 1.900;
+    float rB_O1y = 0.0;
+    float rB_O2y = 0.0;
+    float rB_O3y = 0.0;
+    float rB_O1z = cos(180.0*pi/180.0) * 1.900;
+    float rB_O2z = cos(180.0*pi/180.0) * 1.900;
+    float rB_O3z = cos(180.0*pi/180.0) * 1.900;
+
+    float rB_Clx = 0.0;
+
