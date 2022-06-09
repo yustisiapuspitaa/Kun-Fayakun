8ebf45333662bf24a3ee3e1d1bd996ca2938f5f3
yustisiapuspitaa <106161798+yustisiapuspitaa@users.noreply.github.com>
2022-06-09 07:18:22 +0700

    Update ep-besillloksidadanasamklorida (1).cpp

--- ep-besillloksidadanasamklorida (1).cpp
+++ ep-besillloksidadanasamklorida (1).cpp
@@ -3,3 +3,42 @@
 #include "cmath"
 #include "iomanip"
 #include "fstream"
+
+using namespace std;
+
+float densitas;         /*densitas ini memiliki satuan g cm^-3
+                          dan ini harus diinput dari user */
+// converter dari nilai cm^-3 ke A^3
+float cm3_to_A3 = 1.0E-24;
+float numb_mol;
+float Na = 6.022E23;            // bilangan avogadro
+float Mr_besillloksida = 160.0;  // massa relatif dari besillloksida
+float Mr_asamklorida = 36.46;  // massa relatif dari asam klorida
+int numb_lat;                   // panjang sel simulasi
+int N;                          // jumlah molekul
+float volum;                    // volume dari molekul besillloksida
+float lx, ly, lz;               // panjang sel simulasi terhitung 
+float lat;                      // jarak antar molekul besillloksida
+
+// variabel perhitungan energi potensial
+float sigma = 2.725;            // satuan (A)
+float epsilon = 3.725;          // satuan (kj/mol)
+float rij, rcut, rcut2;
+float Ep, Ep_LJ, A12, B6;
+float rij6, rij12;
+float dx, dy, dz;
+
+int main(){
+    
+    rcut = 3 * sigma;
+    rcut2 = pow(rcut,2);
+    
+    cout << "program menghitung energi potensial dari besillloksidadanasamklorida" << endl;
+    cout << "Masukkan nilai densitas dari besillloksidadanasamklorida (eg. 1.0): ";
+    cin >> densitas;
+
+    numb_mol = densitas * (Na / Mr_besillloksida) * cm3_to_A3;
+    cout << "Masukkan panjang sel simulasi (eg. 5): ";
+    cin >> numb_lat;
+    N = (pow(numb_lat,3)) * 3;
+    volum = (float)N / numb_mol;
