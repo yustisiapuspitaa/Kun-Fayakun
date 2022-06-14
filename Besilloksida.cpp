fda8090c24611e6601a4104fb315e99308102865
La Mardani Putra <106719645+Lamardaniputra@users.noreply.github.com>
2022-06-14 17:11:37 +0700

    Update Besilloksida.cpp

--- Besilloksida.cpp
+++ Besilloksida.cpp
@@ -17,4 +17,17 @@ Int main(){
     float Fe1x[125], Fe2x[125], Fe1y[125], Fe2y[125], Fe1z[125], Fe2z[125];
     float O1x[125], O2x[125], O3X[125], O1y[125], O2y[125], O3y[125], O1z[125], O2z[125], O3z[125]; 
     
-    
+       /*               O    O    O
+                     \\ / \ //
+                      Fe   Fe
+                  molekul besillloksida itu bentuk rigid
+    */
+
+
+    float rB_Fe1x = sin(109.471*pi/180.0) * 1.615;
+    float rB_Fe2x = sin(109.471*pi/180.0) * 1.615;
+    float rB_Fe1y = 0.0;
+    float rB_Fe2y = 0.0;
+    float rB_Fe1z = cos(109.471*pi/180.0) * 1.615;
+    float rB_Fe2z = cos(109.471*pi/180.0) * 1.615;
+
